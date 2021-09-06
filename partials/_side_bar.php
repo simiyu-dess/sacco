b41f977e1e7e1fdbf6cb23f4d9bba1a93f6a3e0d
sims <simiyudennis142@gmail.com>
2021-09-06 11:13:23 +0300

    additional features

--- partials/_side_bar.php
+++ partials/_side_bar.php
@@ -16,23 +16,57 @@
             <i class="fa fa-home"></i>
             <a href="admin_dashboard.php">Dashboard</a>
           </div>
+
+          <div class="sidebar__heading">
+          <i class="fa fa-group fa-fw"></i>
+          <a href="cust_act.php">Members</a>
+          </div>
+          <div class="sidebar__link">
+          <a href="cust_act.php"> Active Members</a>
+          </div>
           
-          <h2><i class="fa fa-home">Loans</i></h2>
+          <div class="sidebar__heading">
+          <i class="fa fa-percent fa-fw"></i>
+          <a href="loans_act.php">Loans</a>
+          </div>
+          <div class="sidebar__link">
+          <a href="loans_act.php">Active Loans</a>
+          </div>
+          <div class="sidebar__link">
+          <a href="loans_pend.php">Pending Loans</a>
+          </div>
           
-          <div class="sidebar__link">
-          <a href="">Search</a>
-          </div>
-          <div class="sidebar__link">
-          <a href="">Active loans</a>
-          </div>
-          <div class="sidebar__link">
-          <a href = "">Pending Loans</a>
-          </div>
-          <div class="sidebar__link">
-          <a href = "">Cleared Loans</a>
-          </div>
-       
-       
         
+        </div>
+        <div class="sidebar__heading">
+        <i class="fa fa-calculator fa-fw"></i>
+        <a href="books_expense.php">Accounting</a>
+        </div>
+        <div class="sidebar__link">
+        <a href="books_expense.php">Expenses</a>
+        </div>
+        <div class="sidebar__link">
+        <a href="books_income">Incomes</a>
+        </div>
+        <div class="sidebar__link">
+        <a href="books_annual.php">Annual Accounts</a>
+        </div>
+        <div class="sidebar__heading">
+        <i class="fa fa-male fa-fw"></i>
+        <a href="empl_curr.php">Employees</a>
+        </div>
+        <div class="sidebar__link">
+        <a href="empl_curr.php">Current Employees</a>
+        </div>
+        <div class="sidebar__link">
+        <a href="empl_past.php">Former Employees</a>
+        </div>
+        <div class="sidebar__link">
+        <a href="empl_new.php">New Employee</a>
+        </div>
+        <div class="sidebar__heading">
+        <i class="fa fa-wrench fa-fw"></i>
+        <a href="set_basic.php">Settings</a>
+        </div>
         </div>
       </div>
--- partials/_top_bar_dashboard.php
+++ partials/_top_bar_dashboard.php
@@ -3,10 +3,10 @@
           <i class="fa fa-bars" aria-hidden="true"></i>
         </div>
         <div class="navbar__left">
-        <a href="cust_search.php"><i class="fa fa-group fa-fw"></i> Members</a>
-        <a href="books_expense.php"><i class="fa fa-calculator fa-fw"></i> Accounting</a>
-        <a href="rep_incomes.php"><i class="fa fa-line-chart fa-fw"></i> Reports</a>
-        <a href="empl_curr.php"><i class="fa fa-male fa-fw"></i> Employees</a>
+        <a href="rep_incomes.php"><i class="fa fa-group fa-fw"></i>Income report</a>
+        <a href="rep_expenses.php"><i class="fa fa-calculator fa-fw"></i>Expenses report</a>
+        <a href="rep_loans.php"><i class="fa fa-line-chart fa-fw"></i>Loans report</a>
+        <a href="rep_revenue.php"><i class="fa fa-male fa-fw"></i>Revenue report </a>
         </div>
         
       </nav>
